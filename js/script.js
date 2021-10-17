e0ab77ae67c7a30c6d9a232fcd5c885a2c7b8926
Nikrbb <81448903+Nikrbb@users.noreply.github.com>
2021-10-17 10:51:13 +0300

    Update
    function simplification

--- js/script.js
+++ js/script.js
@@ -1,11 +1,8 @@
-const userNum = prompt( `Введите число` );
-const userDegree = prompt( `В какую степень возвести число?`, 1 );
+const userNum = +prompt( `Введите число` );
+const userDegree = +prompt( `В какую степень возвести число?`, 1 );
 function degreeCalc( num, degree = 1 ) {
-    if ( ( isNaN( num )  === true ) || ( num  === null ) || ( isNaN( degree )  === true ) ) {
-        return `Awesome ERROR`;
-    } else {
-        return Math.pow( num, degree );
-    }
+    if ( isNaN( num ) || isNaN( degree ) ) return `Awesome ERROR`;
+    return Math.pow( num, degree );
 }
 let calc = degreeCalc( userNum, userDegree );
 alert( calc );
